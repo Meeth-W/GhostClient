bfddef6f4013969b7c2d4db4a1bd9ef06a065954
GitHub Action <action@github.com>
2024-11-23 15:28:43 +0000

    Initialize template repository proper
    Set package name to com.github.meeth-w.ghostclient
    Set project name to GhostClient
    Set modid to ghostclient

--- src/main/java/com/github/meeth-w/ghostclient/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/meeth-w/ghostclient/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.meeth-w.ghostclient.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
